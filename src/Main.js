7e79c8448ef3be248d008a746dc9a330b5d76c9d
ibozorus <korkmazie@gmail.com>
2022-09-19 18:43:37 +0200

    -handleChange und handleSubmit

--- src/Main.js
+++ src/Main.js
@@ -3,18 +3,50 @@ import React from 'react';
 
 export default class Main extends React.Component {
 
-    render () {
-        return(
+    constructor(props) {
+        super(props);
+        this.state = {
+            name : "",
+            password : ""
+
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange({ target }) {
+        const {name, value} = target;
+        this.setState(
+            (prevState) => ({
+                ...prevState,
+                [name]: value
+
+            })
+        )
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.table(this.state);
+    }
+
+
+    render() {
+        return (
             <>
                 <form className="row">
-                    <div className="form-group col-12 col-sm-4">
-                        <label htmlFor='name'><input placeholder="Name" name="name" id="name" type="text"/></label>
-                    </div>
-                    <div className="form-group col-12 col-sm-4">
-                        <label htmlFor='password'><input placeholder="Password" name="password" id="password" type="password"/></label>
-                    </div>
-                    <div className="form-group col-12 col-sm-4">
-                        <button type="submit">Sign in</button>
+                    <div className="form-group col-12 text-center ">
+                        <div>
+                            <label htmlFor='name'><input value={this.state.name} onChange={this.handleChange} placeholder="Name" name="name" id="name" type="text"/></label>
+                        </div>
+                        <div>
+                            <label htmlFor='password'><input value={this.state.password} onChange={this.handleChange} placeholder="Password" name="password" id="password"
+                                                             type="password"/></label>
+                        </div>
+                        <a>
+                            <button className="btn btn-primary"  onClick={this.handleSubmit}>Sign in</button>
+                        </a>
                     </div>
                 </form>
             </>
